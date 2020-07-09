be65bc8923497dd40c5ed0d5b2a631f9189219f2
Alexis Montecillo <ajohanMontecillo@gmail.com>
2020-07-08 22:28:31 -0500

    Finished with main Content

--- js/index.js
+++ js/index.js
@@ -59,3 +59,63 @@ ctaH1.textContent = siteContent.cta['h1'];
 const ctaBtn = document.querySelector('.cta-text button');
 ctaBtn.textContent = siteContent.cta['button'];
 
+const ctaImg = document.getElementById('cta-img')
+ctaImg.src = siteContent.cta["img-src"];
+ctaImg.href = "Circle of code";
+
+
+//Working on main content class .main-content
+// This allows us to access all the elements in the main content without repeating
+//the command.
+const mainContent = document.getElementsByClassName("main-content");
+console.log(mainContent[0]);
+
+const mainContentFeatureH4 = mainContent[0].querySelector(".text-content h4");
+//The code below allows us to access the elements in the main content array
+//This is because you cannot write main-content on JS because of the dash
+let siteContentMainContent = siteContent["main-content"];
+mainContentFeatureH4.textContent = siteContentMainContent["features-h4"];
+
+const mainContentFeaturesContent = mainContent[0].querySelector('p');
+mainContentFeaturesContent.textContent = siteContentMainContent["features-content"];
+
+//about
+const mainContentClassTextContent = mainContent[0].getElementsByClassName('text-content');
+const aboutH4 = mainContentClassTextContent[1].querySelector('h4');
+aboutH4.textContent = siteContentMainContent["about-h4"];
+
+const aboutContent = mainContentClassTextContent[1].querySelector("p");
+aboutContent.textContent = siteContentMainContent["about-content"];
+
+//Middle image created by using Id
+const middleImg = document.getElementById("middle-img");
+middleImg.src = siteContentMainContent["middle-img-src"];
+
+//service 
+const serviceH4 = mainContentClassTextContent[2].querySelector("h4");
+serviceH4.textContent = siteContentMainContent["services-h4"];
+
+const serviceContent = mainContentClassTextContent[2].querySelector("p")
+serviceContent.textContent = siteContentMainContent["services-content"];
+
+//Product
+const productH4 = mainContentClassTextContent[3].querySelector("h4");
+productH4.textContent = siteContentMainContent["product-h4"];
+
+const productContent = mainContentClassTextContent[3].querySelector("p");
+productContent.textContent = siteContentMainContent["product-content"];
+
+//vision
+const visionH4 = mainContentClassTextContent[4].querySelector("h4");
+visionH4.textContent = siteContentMainContent["vision-h4"];
+
+const visionContent = mainContentClassTextContent[4].querySelector("p");
+visionContent.textContent = siteContentMainContent["vision-content"];
+
+// finished with Main contetn
+
+
+//
+
+
+
